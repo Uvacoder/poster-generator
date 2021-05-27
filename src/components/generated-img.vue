97578c13e02e9b7d04c9b39d18f9b1a8b980a8d0
Tim Benniks <tbenniks@gmail.com>
2021-05-27 18:15:02 +0200

    better archi

--- src/components/generated-img.vue
+++ src/components/generated-img.vue
@@ -1,54 +1,93 @@
 <template>
-  <cld-image publicId="tt-bg" style="width: 800px">
-    <cld-transformation width="1920" quality="auto" />
+  <div class="image-rendered">
+    <p>Save this image and use it!</p>
+    <template v-if="data.template === 'tt'">
+      <cld-image publicId="uniform-tt-bg" style="width: 800px">
+        <cld-transformation width="1920" quality="auto" />
 
-    <cld-transformation
-      :overlay="{
-        fontFamily: 'montserrat',
-        fontSize: 40,
-        fontWeight: 'bold',
-        text: subtitle,
-      }"
-      gravity="north_west"
-      y="303"
-      x="328"
-      color="#FBA633"
-      v-if="subtitle !== ''"
-    />
+        <cld-transformation
+          :overlay="{
+            fontFamily: 'montserrat',
+            fontSize: 40,
+            fontWeight: 'bold',
+            text: encodeURIComponent(data.subtitle),
+          }"
+          gravity="north_west"
+          y="303"
+          x="328"
+          color="#FBA633"
+          v-if="data.subtitle !== ''"
+        />
 
-    <cld-transformation
-      :overlay="{
-        fontFamily: 'montserrat',
-        fontWeight: 'bold',
-        fontSize: 100,
-        text: maintitle.toUpperCase(),
-      }"
-      gravity="north_west"
-      y="371"
-      x="328"
-      color="#fff"
-      crop="fit"
-      width="1400"
-      v-if="maintitle !== ''"
-    />
+        <cld-transformation
+          :overlay="{
+            fontFamily: 'montserrat',
+            fontWeight: 'bold',
+            fontSize: 100,
+            text: encodeURIComponent(data.maintitle.toUpperCase()),
+          }"
+          gravity="north_west"
+          y="371"
+          x="328"
+          color="#fff"
+          crop="fit"
+          width="1400"
+          v-if="data.maintitle !== ''"
+        />
 
-    <cld-transformation
-      v-if="dudes"
-      overlay="dudes"
-      gravity="north_west"
-      width="406"
-      y="654"
-      x="328"
-    />
-  </cld-image>
+        <cld-transformation
+          v-if="data.dudes"
+          overlay="uniform-tt-dudes"
+          gravity="north_west"
+          width="406"
+          y="654"
+          x="328"
+        />
+      </cld-image>
+    </template>
+
+    <template v-if="data.template === 'pm'">
+      <cld-image publicId="uniform-pm-bg" style="width: 800px">
+        <cld-transformation width="1920" quality="high" />
+
+        <cld-transformation
+          :overlay="{
+            fontFamily: 'montserrat',
+            fontSize: 40,
+            fontWeight: 'bold',
+            text: encodeURIComponent(data.subtitle),
+          }"
+          gravity="north_west"
+          y="303"
+          x="328"
+          color="#FBA633"
+          v-if="data.subtitle !== ''"
+        />
+
+        <cld-transformation
+          :overlay="{
+            fontFamily: 'montserrat',
+            fontWeight: 'bold',
+            fontSize: 100,
+            text: encodeURIComponent(data.maintitle.toUpperCase()),
+          }"
+          gravity="north_west"
+          y="371"
+          x="328"
+          color="#fff"
+          crop="fit"
+          width="1400"
+          v-if="data.maintitle !== ''"
+        />
+      </cld-image>
+    </template>
+  </div>
 </template>
 <script>
 export default {
   name: "GeneratedImg",
   props: {
-    subtitle: { type: String },
-    maintitle: { type: String },
-    dudes: { type: Boolean },
+    data: { type: Object, required: true },
   },
 };
 </script>
--- src/components/generator.vue
+++ src/components/generator.vue
@@ -1,39 +1,109 @@
 <template>
   <div class="poster-creator">
-    <form @submit.prevent="onSubmit()">
-      <div class="form-item">
-        <label for="subtitle">Sub Title</label>
-        <input
-          type="text"
-          id="subtitle"
-          placeholder="subtitle here"
-          v-model="subtitle"
-        />
+    <h1>Poster Generator</h1>
+    <div class="fake-form" v-if="showForm">
+      <div class="form-item form-item-template">
+        <label for="subtitle">Template</label>
+        <select id="template" v-model="template">
+          <option value="tt" selected>Jamstack Fridays with T&T</option>
+          <option value="pm">Product Meetup</option>
+        </select>
+      </div>
+      <div class="template" v-if="template === 'tt'">
+        <div class="form-item-wrap">
+          <h2>Jamstack Fridays with T&T</h2>
+          <div class="form-item">
+            <label for="subtitle">Sub Title</label>
+            <input
+              type="text"
+              id="subtitle"
+              placeholder="Subtitle here"
+              v-model="tt.subtitle"
+            />
+          </div>
+          <div class="form-item">
+            <label for="maintitle">Main Title</label>
+            <input
+              type="text"
+              id="maintitle"
+              placeholder="Main title here"
+              v-model="tt.maintitle"
+            />
+          </div>
+          <div class="form-item">
+            <label for="dudes">
+              <input type="checkbox" id="dudes" v-model="tt.dudes" /> Show dudes
+            </label>
+          </div>
+        </div>
+        <div class="logo-wrap">
+          <img
+            width="300"
+            src="../assets/tt.png"
+            alt="Jamstack Fridays with T&T"
+          />
+        </div>
+      </div>
+      <div class="template" v-if="template === 'pm'">
+        <div class="form-item-wrap">
+          <h2>Product Meetup</h2>
+
+          <div class="form-item">
+            <label for="maintitle">Main Title</label>
+            <input
+              type="text"
+              id="maintitle"
+              placeholder="Main title here"
+              v-model="pm.maintitle"
+            />
+          </div>
+          <div class="form-item">
+            <label for="subtitle">Sub Title</label>
+            <input
+              type="text"
+              id="subtitle"
+              placeholder="Subtitle here"
+              v-model="pm.subtitle"
+            />
+          </div>
+          <div class="form-item">
+            <label for="logo">
+              <input type="checkbox" value="logo" id="logo" v-model="pm.logo" />
+              Show Logo
+            </label>
+          </div>
+          <div class="form-item">
+            <label>People</label>
+
+            <label for="lars">
+              <input
+                type="checkbox"
+                value="lars"
+                id="lars"
+                v-model="pm.people"
+              />
+              Lars
+            </label>
+            <label for="tim">
+              <input type="checkbox" value="tim" id="tim" v-model="pm.people" />
+              Tim
+            </label>
+          </div>
+        </div>
+        <div class="logo-wrap">
+          <img
+            width="150"
+            src="../assets/uniform-logo.png"
+            alt="Jamstack Fridays with T&T"
+          />
+        </div>
       </div>
       <div class="form-item">
-        <label for="maintitle">Main Title</label>
-        <input
-          type="text"
-          id="maintitle"
-          placeholder="main title here"
-          v-model="maintitle"
-        />
+        <button @click="createImage(template)">Generate poster image</button>
       </div>
-      <div class="form-item">
-        <label for="dudes">
-          <input type="checkbox" id="dudes" v-model="dudes" /> Show dudes
-        </label>
-      </div>
-      <div class="form-item">
-        <button type="submit">generate image</button>
-      </div>
-    </form>
-    <div class="poster-output" v-if="formDataForImage.maintitle !== ''">
-      <generated-img
-        :subtitle="formDataForImage.subtitle"
-        :maintitle="formDataForImage.maintitle"
-        :dudes="formDataForImage.dudes"
-      />
+    </div>
+    <div class="poster-output" v-if="!showForm">
+      <generated-img :data="imageGeneratorProps" />
     </div>
   </div>
 </template>
@@ -48,23 +118,28 @@ export default {
   },
   data() {
     return {
-      subtitle: "",
-      maintitle: "",
-      dudes: true,
-      formDataForImage: {
+      showForm: true,
+      template: "tt",
+      tt: {
         subtitle: "",
         maintitle: "",
         dudes: true,
+      },
+      pm: {
+        subtitle: "",
+        maintitle: "",
+        people: [],
+        logo: true,
       },
     };
   },
 
   methods: {
-    onSubmit() {
-      this.formDataForImage = {
-        subtitle: this.subtitle,
-        maintitle: this.maintitle,
-        dudes: this.dudes,
+    createImage(template) {
+      this.showForm = false;
+      this.imageGeneratorProps = {
+        template,
+        ...this[template],
       };
     },
   },
@@ -75,16 +150,44 @@ export default {
 .poster-creator {
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  color: #2c3e50;
+  color: #1d3557;
 
   max-width: 800px;
   margin: 5em auto 0;
   width: 90%;
 }
 
-form {
-  border: 1px solid #eee;
-  padding: 1rem 1rem 0 1rem;
+.poster-output {
+  margin: 1rem 0 0 0;
+}
+
+h2 {
+  margin: 0 0 1rem 0;
+}
+
+.fake-form {
+  border: 1px solid #ddd;
+  padding: 1rem;
+  background: #f1f1f1;
+  border-radius: 0.3rem;
+}
+
+.template {
+  padding: 1rem;
+  border: 1px solid #ddd;
+  background: #fff;
+  border-radius: 0.3rem;
+  margin: 0 0 1rem 0;
+  display: flex;
+}
+
+.logo-wrap {
+  flex: 1;
+  width: 100%;
+  align-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 label {
@@ -96,9 +199,39 @@ label {
   margin: 0 0 2rem 0;
 }
 
-input[type="text"] {
-  border: 1px solid #eee;
+.form-item:last-child {
+  margin: 0;
+}
+
+.form-item-template {
+  margin: 0 0 1rem 0;
+}
+
+input,
+select,
+button {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  border-radius: 0.3rem;
+}
+
+input[type="text"],
+select {
+  border: 1px solid #ddd;
   padding: 1rem;
   width: 300px;
+}
+
+button {
+  border: none;
+  background: #1d3557;
+  color: #fff;
+  padding: 1rem;
+  font-size: 1rem;
+  cursor: pointer;
+}
+
+button:hover {
+  background: #161e2e;
 }
 </style>
